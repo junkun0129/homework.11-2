319f7c74dbc14cb8d31dec8a764f7cacaf7bb6be
unknown <penpenetai@icloud.com>
2022-11-02 22:13:26 -0700

    perfect version

--- CardComponent.jsx
+++ CardComponent.jsx
@@ -1,21 +1,21 @@
-// function Iwatani(icon, title){
-//     return (
-//     <div class="button">
-//       <div class="icon">
-//         <i class={"fa-fa-"+icon}></i>
-//       </div>
-//       <span>{title}</span>
-//     </div>
-//     )
-// }
-
-Iwatani((icon, title)=>{
+function Iwatani({icon, title}){
     return (
-        <div class="button">
-          <div class="icon">
-            <i class={"fa-fa-"+icon}></i>
-          </div>
-          <span>{title}</span>
-        </div>
-        )
-})
+    <div class="button">
+      <div class="icon">
+        <i class={"fa fa-"+icon}></i>
+      </div>
+      <span>{title}</span>
+    </div>
+    )
+}
+
+// Iwatani((icon, title)=>{
+//     return (
+//         <div class="button">
+//           <div class="icon">
+//             <i class={"fa-fa-"+icon}></i>
+//           </div>
+//           <span>{title}</span>
+//         </div>
+//         )
+// })
